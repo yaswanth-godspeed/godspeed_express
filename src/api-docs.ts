f522998bedeb03b966e847aad7d27d9d809000cc
YaswanthBonumaddi9 <yaswanth@godspeed.systems>
2023-10-24 16:37:39 +0530

    first

---
+++ src/api-docs.ts
@@ -0,0 +1,124 @@
+/*
+ * You are allowed to study this software for learning and local * development purposes only. Any other use without explicit permission by Mindgrep, is prohibited.
+ * © 2022 Mindgrep Technologies Pvt Ltd
+ */
+import loadYaml from '@godspeedsystems/core/dist/core/yamlLoader';
+import yaml from 'yaml';
+import { PlainObject } from '@godspeedsystems/core';
+import { logger } from '@godspeedsystems/core/dist/logger';
+import fs from 'fs-extra';
+import path from 'path';
+import swaggerCommonPart from './basic-specs';
+
+// add it here, because of circular dependency of logger
+function removeNulls(obj: PlainObject) {
+  const isArray = Array.isArray(obj);
+  for (const k of Object.keys(obj)) {
+    if (obj[k] === null) {
+      if (isArray) {
+        //@ts-ignore
+        obj.splice(k, 1);
+      } else {
+        delete obj[k];
+      }
+    } else if (typeof obj[k] === 'object') {
+      removeNulls(obj[k]);
+    }
+    //@ts-ignore
+    if (isArray && obj.length === k) {
+      removeNulls(obj);
+    }
+  }
+  return obj;
+}
+
+export default async function generateSchema(
+  eventsFolderPath: string,
+  definitionsFolderPath: string,
+  configPath: string
+): Promise<PlainObject> {
+  const eventsSchema: PlainObject = await loadEventsYaml(eventsFolderPath);
+  const definitions: PlainObject = await loadYaml(definitionsFolderPath, false);
+
+  let finalSpec: PlainObject = {};
+  let swaggerSpecBase = JSON.parse(JSON.stringify(swaggerCommonPart));
+
+  try {
+    const swaggerfilePath = path.resolve(path.join(configPath, 'swagger.json'));
+    if (fs.existsSync(swaggerfilePath)) {
+      let swaggerSpecCustom = JSON.parse(fs.readFileSync(swaggerfilePath, 'utf-8'));
+      swaggerSpecBase = { ...swaggerSpecBase, info: { ...swaggerSpecCustom.info } };
+    }
+
+    finalSpec = swaggerSpecBase;
+
+    Object.keys(eventsSchema).forEach((event: any) => {
+      let apiEndPoint = event.split('.')[0];
+      apiEndPoint = apiEndPoint.replaceAll(/:([^\/]+)/g, '{$1}'); //We take :path_param. OAS3 takes {path_param}
+      const method = event.split('.')[2];
+      const eventSchema = eventsSchema[event];
+
+      //Initialize the schema for this method, for given event
+      let methodSpec: PlainObject = {
+        summary: eventSchema.summary,
+        description: eventSchema.description,
+        requestBody: eventSchema.body || eventSchema.data?.schema?.body,
+        parameters:
+          eventSchema.parameters ||
+          eventSchema.params ||
+          eventSchema.data?.schema?.params,
+        responses: eventSchema.responses,
+      };
+
+      //Set it in the overall schema
+      finalSpec.paths[apiEndPoint] = {
+        ...finalSpec.paths[apiEndPoint],
+        [method]: methodSpec,
+      };
+    });
+    // add definitions{models} in swagger specs
+    finalSpec.definitions = definitions;
+  } catch (error) {
+    logger.error(error);
+  }
+
+  removeNulls(finalSpec);
+  return finalSpec;
+}
+
+async function loadEventsYaml(path: string) {
+  try {
+    return await loadYaml(path, true);
+  } catch (ex) {
+    logger.error('Error in reading events YAMLs', ex);
+    process.exit(1);
+  }
+}
+
+if (require.main === module) {
+  const eventPath = '/workspace/development/app/src/events';
+  const definitionsPath = '/workspace/development/app/src/definitions';
+  const configPath = '/workspace/development/app/config';
+  generateSchema(eventPath, definitionsPath, configPath)
+    .then((schema) => {
+      fs.outputFile(
+        '/workspace/development/app/docs/api-doc.yaml',
+        yaml.stringify(schema),
+        (err) => {
+          if (err) {
+            logger.error(
+              'Error in generating /workspace/development/app/docs/api-doc.yaml file %o',
+              err
+            );
+          } else {
+            logger.info(
+              '/workspace/development/app/docs/api-doc.yaml file is saved!'
+            );
+          }
+        }
+      );
+    })
+    .catch((e) => {
+      logger.error('Error: %o', e);
+    });
+}
---
+++ src/index.ts
@@ -0,0 +1,92 @@
+import { PlainObject, GSActor, GSCloudEvent, GSStatus, GSEventSource, GSDataSource, GSContext } from "@godspeedsystems/core";
+import express from "express";
+import bodyParser from 'body-parser';
+import _ from "lodash";
+import { dirname } from  'path';
+import path from 'path';
+import swaggerUI from 'swagger-ui-express';
+import generateSchema from './api-docs'
+class EventSource extends GSEventSource {
+  async initClient(): Promise<PlainObject> {
+    const app = express();
+    const {
+      port = 3000,
+      request_body_limit = 50 * 1024 * 1024,
+      file_size_limit = 50 * 1024 * 1024
+    } = this.config;
+
+    app.use(bodyParser.urlencoded({ extended: true, limit: request_body_limit }));
+    app.use(bodyParser.json({ limit: file_size_limit }));
+    app.listen(port);
+  
+
+    generateSchema("/home/ayush/yash/src/events", "/home/ayush/yash/src/definitions", "/home/ayush/yash/config")
+    .then((schema:any) => {
+      app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(schema));
+    })
+    .catch((e:any) => {
+      process.exit(1);
+    });
+
+    return app;
+  }
+
+  subscribeToEvent(eventRoute: string, eventConfig: PlainObject, processEvent: (event: GSCloudEvent, eventConfig: PlainObject) => Promise<GSStatus>): Promise<void> {
+    const routeSplit = eventRoute.split('.');
+    const httpMethod: string = routeSplit[1];
+    const endpoint = routeSplit[2].replace(/{(.*?)}/g, ':$1');
+    const app: express.Express = this.client as express.Express;
+
+    //@ts-ignore
+    app[httpMethod](endpoint, async (req: express.Request, res: express.Response) => {
+      const gsEvent: GSCloudEvent = EventSource.createGSEvent(req, endpoint)
+      const status: GSStatus = await processEvent(gsEvent, eventConfig);
+      res
+        .status(status.code || 200)
+        // if data is a integer, it takes it as statusCode, so explicitly converting it to string
+        .send(Number.isInteger(status.data) ? String(status.data) : status.data);
+    });
+    return Promise.resolve();
+  }
+
+  static createGSEvent(req: express.Request, endpoint: string) {
+    const reqProp = _.omit(req, [
+      '_readableState',
+      'socket',
+      'client',
+      '_parsedUrl',
+      'res',
+      'app'
+    ]);
+    const reqHeaders = _.pick(req, ['headers']);
+    let data = { ...reqProp, ...reqHeaders };
+
+    const event: GSCloudEvent = new GSCloudEvent(
+      'id',
+      endpoint,
+      new Date(),
+      'http',
+      '1.0',
+      data,
+      'REST',
+      new GSActor('user'),
+      {}
+    );
+
+    return event;
+  }
+}
+
+const SourceType = 'ES';
+const Type = 'express'; // this is the loader file of the plugin, So the final loader file will be `types/${Type.js}`
+const CONFIG_FILE_NAME = 'http'; // in case of event source, this also works as event identifier, and in case of datasource works as datasource name
+const DEFAULT_CONFIG = { port: 3000 };
+
+export {
+  EventSource,
+  SourceType,
+  Type,
+  CONFIG_FILE_NAME,
+  DEFAULT_CONFIG
+};
+
